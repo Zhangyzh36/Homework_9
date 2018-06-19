44434716d37cf0f55f7ce46c6281c936b6f07757
Zhangyzh36 <771235191@qq.com>
2018-06-20 00:12:05 +0800

    Add files via upload

---
+++ VirtualButtonEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler{
+	public GameObject vb;
+	// Use this for initialization
+	void Start () {
+		VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour> ();
+		if (vbb) {
+			vbb.RegisterEventHandler (this);
+		}
+	}
+	
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb) {
+		Debug.Log ("pressed");
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb) {
+		Debug.Log ("released");
+	}
+}
